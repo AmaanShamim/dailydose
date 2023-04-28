ea37df0b4ce3d63467ad72eebc7388e4483ed167
Amaan <amaans1912@gmail.com>
2023-04-28 21:54:37 +0530

    Pagination implementation to limit content rendered

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: 330 }}>
+        <div className="card" style={{ width: "20rem" }}>
           <img src={!imageUrl?"notAvailable.png":imageUrl} className="card-img-top" alt="..." style={{height: 180}} />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
